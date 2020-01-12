d8487d607a7087e25ed14472bca4407f1687e65c
Kouyu <kou120418@gmail.com>
2020-01-12 15:37:48 +0800

    first commit

---
+++ dist/script.js
@@ -0,0 +1,14 @@
+var s = skrollr.init()
+
+$(window).scroll(function(evt){
+  if ($(window).scrollTop()>0){
+    $(".navbar").removeClass("navbar-top");
+    $(".navbar").addClass("bg-light");
+    $(".navbar").addClass("navbar-light");
+  }
+  else{
+    $(".navbar").addClass("navbar-top");
+    $(".navbar").removeClass("bg-light");
+    $(".navbar").removeClass("navbar-light");
+  }
+});
