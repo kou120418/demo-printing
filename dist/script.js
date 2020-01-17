5db48b091c2bdccc85ca3b1659f77f26132cebe2
Kouyu <kou120418@gmail.com>
2020-01-17 20:26:53 +0800

    add delay js

--- dist/script.js
+++ dist/script.js
@@ -1,14 +1,20 @@
 var s = skrollr.init()
 
-$(window).scroll(function(evt){
-  if ($(window).scrollTop()>0){
+$(window).scroll(function (evt) {
+  if ($(window).scrollTop() > 0) {
     $(".navbar").removeClass("navbar-top");
     $(".navbar").addClass("bg-light");
     $(".navbar").addClass("navbar-light");
-  }
-  else{
+  } else {
     $(".navbar").addClass("navbar-top");
     $(".navbar").removeClass("bg-light");
     $(".navbar").removeClass("navbar-light");
   }
+});
+
+$(document).on('click', 'a', function (event) {
+  event.preventDefault();
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
 });
